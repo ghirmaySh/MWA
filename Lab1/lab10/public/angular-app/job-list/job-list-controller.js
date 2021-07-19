61924efa7a338463822f393bcbee7d643f4d6d1d
ghirmaySh <81131870+ghirmaySh@users.noreply.github.com>
2021-07-18 19:18:42 -0500

    Delete Lab1 directory

--- Lab1/lab10/public/angular-app/job-list/job-list-controller.js
+++
@@ -1,9 +0,0 @@
-angular.module("meanJobSearch").controller("JobsController", JobsController);
-
-function JobsController(JobsDataFactory) {
-  const vm = this;
-  vm.title = "Job Search App";
-  JobsDataFactory.getAll().then(function (response) {
-    vm.jobs = response;
-  });
-}
